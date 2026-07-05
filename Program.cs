a0e5c2606e559ec4afce0f48ae772924ce86664d
Tsurumi Tatsuya <93573854+tatsuya-tsurumi@users.noreply.github.com>
2026-07-05 12:04:37 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var todos = new List<object>
+{
+    new
+    {
+        Id = 1,
+        Title = "ASP.NET Coreを勉強する",
+        IsCompleted = false
+    },
+    new
+    {
+        Id = 2,
+        Title = "Swaggerを確認する",
+        IsCompleted = true
+    }
+};
+
+app.MapGet("/todos", () => todos)
+    .WithName("GetTodos");
+
+app.Run();
